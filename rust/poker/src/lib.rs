0e87cde9d0993efb8c716e5f5e4070cdfa3e48c2
Silcet <camorga1@gmail.com>
2022-08-07 20:24:46 +0000

    Progressing with poker

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::BTreeMap};
 
 /// Given a list of poker hands, return a list of those hands which win.
 ///
@@ -8,20 +8,33 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
     hands
         .iter()
         .map(|hand| PokerHand::new(hand))
+        .inspect(|x| println!("Poker hand: {:?}", x))
         .fold(Vec::<PokerHand>::new(), |mut winning_hands, hand| {
+            println!("Got hand: {:?}", hand);
             if winning_hands.is_empty() {
+                println!("List was empty.");
                 winning_hands.push(hand);
             } else {
                 match winning_hands.first().unwrap().partial_cmp(&hand) {
-                    Some(Ordering::Greater) => {}
-                    Some(Ordering::Equal) => {}
-                    Some(Ordering::Less) => {}
+                    Some(Ordering::Greater) => {
+                        println!("It's bigger");
+                        winning_hands.clear();
+                        winning_hands.push(hand);
+                    }
+                    Some(Ordering::Equal) => {
+                        println!("It's the same");
+                        winning_hands.push(hand);
+                    }
+                    Some(Ordering::Less) => {
+                        println!("It's less");
+                    }
                     None => panic!("Can't compare hands"),
                 }
             }
             winning_hands
         })
         .iter()
+        .inspect(|x| println!("Winning hand: {:?}", x))
         .map(|poker_hand| poker_hand.cards)
         .collect()
 }
@@ -29,13 +42,13 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
 #[derive(Ord, Eq, PartialOrd, PartialEq, Debug)]
 enum Hands {
     StraightFlush(u8),
-    FourOfAKind(u8),
+    FourOfAKind(u8, u8),
     FullHouse(u8, u8),
-    Flush(u8),
+    Flush(u8, u8),
     Straight(u8),
     ThreeOfAKind(u8),
     TwoPair(u8, u8),
-    OnePair(u8),
+    OnePair(u8, u8),
     HighCard(u8),
 }
 
@@ -47,17 +60,61 @@ struct PokerHand<'a> {
 
 impl<'a> PokerHand<'a> {
     pub fn new(cards: &'a str) -> Self {
+        let cards_map = Self::cards_to_map(cards);
+        println!("Cards: {}", cards);
+        println!("Cards map: {:?}", cards_map);
         Self {
             cards,
-            hand: Self::parse_cards(cards),
+            hand: Self::parse_cards(cards_map),
         }
     }
 
-    fn parse_cards(cards: &'a str) -> Hands {
-        if cards.contains('A') {
-            Hands::StraightFlush(1)
-        } else if cards.contains('K') {
-            Hands::HighCard(10)
+    fn cards_to_map(cards: &'a str) -> BTreeMap<u8, Vec<char>> {
+        cards
+            .split(' ')
+            .fold(BTreeMap::<u8, Vec<char>>::new(), |mut map, card| {
+                let chars = card.chars().rev().collect::<String>();
+                let split_chars = chars.split_at(1);
+                let number = Self::parse_number(split_chars.1);
+                let suit = split_chars.0.chars().next().unwrap();
+
+                let entry = map.get_mut(&number);
+                if let Some(value) = entry {
+                    value.push(suit);
+                } else {
+                    map.insert(number, [suit].into_iter().collect());
+                }
+                map
+            })
+    }
+
+    fn parse_number(number: &str) -> u8 {
+        match number {
+            "A" => 1,
+            "2" => 2,
+            "3" => 3,
+            "4" => 4,
+            "5" => 5,
+            "6" => 6,
+            "7" => 7,
+            "8" => 8,
+            "9" => 9,
+            "10" => 10,
+            "J" => 11,
+            "Q" => 12,
+            "K" => 13,
+            _ => 0,
+        }
+    }
+
+    fn parse_cards(cards: BTreeMap<u8, Vec<char>>) -> Hands {
+        if cards.len() == 2 {
+            let keys: Vec<&u8> = cards.keys().collect();
+            match (cards[keys[0]].len(), cards[keys[1]].len()) {
+                (4, 1) => Hands::FourOfAKind(*keys[0], *keys[1]),
+                (1, 4) => Hands::FourOfAKind(*keys[1], *keys[0]),
+                _ => Hands::OnePair(2, 1),
+            }
         } else {
             Hands::HighCard(1)
         }
